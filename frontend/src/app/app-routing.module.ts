58b1009d7582741fa44524e5e40db1bdff7127bd
mondayishere01 <deveshpandey.pear@gmail.com>
2024-06-05 03:00:14 +0530

    added the incomplete comming soon part

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommingSoonComponent } from './pages/comming-soon/comming-soon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'comming-soon', pathMatch: 'full' },
+  { path: 'comming-soon', component: CommingSoonComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommingSoonComponent } from './pages/comming-soon/comming-soon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommingSoonComponent
   ],
   imports: [
     BrowserModule,
